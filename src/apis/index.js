8adb3df923b34352031cb6b638782d9c04cf9227
thanhtienle0601 <thanhtienle0601@gmail.com>
2024-10-09 13:11:48 +0700

    DND move columns

--- src/apis/index.js
+++ src/apis/index.js
@@ -5,3 +5,21 @@ export const fetchBoardDetailsAPI = async (boardId) => {
   const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
   return response.data
 }
+
+export const updateBoardAPI = async (boardId, updateData) => {
+  const response = await axios.put(
+    `${API_ROOT}/v1/boards/${boardId}`,
+    updateData
+  )
+  return response.data
+}
+
+export const createColumnAPI = async (columnData) => {
+  const response = await axios.post(`${API_ROOT}/v1/columns`, columnData)
+  return response.data
+}
+
+export const createCardAPI = async (cardData) => {
+  const response = await axios.post(`${API_ROOT}/v1/cards`, cardData)
+  return response.data
+}
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,22 +3,87 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardContent from './BoardContent/BoardContent'
 import BoardBar from './BoardBar/BoardBar'
 import { mockData } from '~/apis/mock-data'
-import { fetchBoardDetailsAPI } from '~/apis/index'
+import {
+  createCardAPI,
+  createColumnAPI,
+  fetchBoardDetailsAPI,
+  updateBoardAPI
+} from '~/apis/index'
 import { useEffect, useState } from 'react'
+import { isEmpty } from 'lodash'
+import { generatePlaceHolderCard } from '~/utils/formatters'
 
 const Board = () => {
   const [board, setBoard] = useState(null)
+
   useEffect(() => {
     const boardId = '66fbccd45370a6270fe3467a'
     fetchBoardDetailsAPI(boardId).then((board) => {
+      board.columns.forEach((column) => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceHolderCard(column)]
+          column.cardOrderIds = [generatePlaceHolderCard(column)._id]
+        }
+      })
       setBoard(board)
     })
   }, [])
+
+  const createNewColumn = async (columnData) => {
+    const createdColumn = await createColumnAPI({
+      ...columnData,
+      boardId: board._id
+    })
+
+    createdColumn.cards = [generatePlaceHolderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceHolderCard(createdColumn)._id]
+
+    const newBoard = { ...board }
+    newBoard.columns.push(createdColumn)
+    newBoard.columnOrderIds.push(createdColumn._id)
+    setBoard(newBoard)
+  }
+
+  const createNewCard = async (cardData) => {
+    const createdCard = await createCardAPI({
+      ...cardData,
+      boardId: board._id
+    })
+
+    const newBoard = { ...board }
+    const columnToUpdate = newBoard.columns.find(
+      (column) => column._id === createdCard.columnId
+    )
+    if (columnToUpdate) {
+      columnToUpdate.cards.push(createdCard)
+      columnToUpdate.cardOrderIds.push(createdCard._id)
+    }
+    setBoard(newBoard)
+  }
+
+  const moveColumns = async (dndOrderedColumns) => {
+    const dndOrderedColumnIds = dndOrderedColumns.map((c) => c._id)
+
+    const newBoard = { ...board }
+    newBoard.columns = dndOrderedColumns
+    newBoard.columnOrderIds = dndOrderedColumnIds
+    setBoard(newBoard)
+
+    await updateBoardAPI(newBoard._id, {
+      columnOrderIds: dndOrderedColumnIds
+    })
+  }
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData.board} />
-      <BoardContent board={mockData.board} />
+      <BoardBar board={board} />
+      <BoardContent
+        board={board}
+        createNewColumn={createNewColumn}
+        createNewCard={createNewCard}
+        moveColumns={moveColumns}
+      />
     </Container>
   )
 }
