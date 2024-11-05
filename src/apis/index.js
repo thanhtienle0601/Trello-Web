e72d1639250fffc7cfe3b0283d451983da956b5c
thanhtienle0601 <thanhtienle0601@gmail.com>
2024-11-05 15:09:59 +0700

    Refactor board fetching logic and improve board display with API integration

--- src/apis/index.js
+++ src/apis/index.js
@@ -84,3 +84,10 @@ export const refreshTokenAPI = async () => {
   )
   return response.data
 }
+
+export const fetchBoardsAPI = async (searchPath) => {
+  const response = await authorizeAxiosInstance.get(
+    `${API_ROOT}/v1/boards${searchPath}`
+  )
+  return response.data
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,6 @@ if (process.env.BUILD_MODE === 'prod') {
   apiRoot = 'https://trello-api-lkhn.onrender.com'
 }
 export const API_ROOT = apiRoot
+
+export const DEFAULT_PAGE = 1
+export const DEFAULT_ITEM_PER_PAGE = 12
